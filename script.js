ba66dcdc6540461972c8b58762059c172bf9740e
Vlad Shcherbina <vlad.shcherbina@gmail.com>
2013-11-08 23:51:24 +0100

    add rudimentary language picker

--- script.js
+++ script.js
@@ -73,17 +73,23 @@ function RootController($scope, angularFire, angularFireAuth) {
 }
 
 function DictsController($scope) {
+  $scope.language_pair = 'en-de';
+  $scope.left_lang = 'en';
+  $scope.right_lang = 'de';
+
   $scope.q_to_add = "";
   $scope.a_to_add = "";
 
   $scope.add = function(q, a) {
     console.log("q and a: ", q, a);
 
-    var words = $scope.data.dicts["en-de"].words;
+    var words =
+        $scope.data.dicts[$scope.left_lang + '-' + $scope.right_lang].words;
     if (!(q in words)) words[q] = {};
     words[q][a] = makeWordInfo();
 
-    words = $scope.data.dicts["de-en"].words;
+    words =
+        $scope.data.dicts[$scope.right_lang + '-' + $scope.left_lang].words;
     if (!(a in words)) words[a] = {};
     words[a][q] = makeWordInfo();
 
